e874f9712275ea9b59eb6f3710cbc637806dedeb
lucasmalb <139927165+lucasmalb@users.noreply.github.com>
2024-05-22 14:16:00 -0300

    cambios

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,114 @@
-const featured = document.querySelector("section.featured .featured-container")
-const best = document.querySelector("section.best .best-container")
+//const featured = document.querySelector("section.featured .featured-container")
+//const best = document.querySelector("section.best .best-container")
 const dropdown = document.querySelector("header nav .dropdown")
 const sections = document.querySelectorAll(".fade")
 
-for (let i = 0; i < 19; i++) {
-    const titles = ["The Beekeeper", "Badland Hunters", "The Marvels", "Wonka", "Aquaman and the Lost Kingdom", "Migration", "Sixty Minutes", "Wish", "The Masked Saint", "Due Justice", "Orion and the Dark", "Genghis Khan", "Lift", "Attack", "Mutant Ghost Wargirl", "Poor Things", "The 5", "Truck: Locked in", "Anyone but you"]
+/*Conexion con API the movideDB*/
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZjg1ZjA1YzMzYmQ4N2UyYjNhYzZhZTljY2Q1ZDIzMyIsInN1YiI6IjY2NGRkZjdhNTE5NTYwMmQ0YjQzY2Q1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6VPi2NHafgsMnKOggFX4w6u0toowpwUnUdMa05a5MQI'
+    }
+  };
+//Asignamos que arrancamos en la pagina 1
+  let currentPage = 1;
+  //Traemos las tendencias de hoy
+  async function fetchMovies(pageNumber) {
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pageNumber}&sort_by=popularity.desc`, options);
+        const data = await response.json();
+
+        // Aquí extraemos los resultados
+        const movies = data.results;
+        const featuredContainer = document.querySelector(".featured-container");
+        //Borramos los containers para que en cada pagina arranquen vacios
+        featuredContainer.innerHTML = "";
+        // Iteramos sobre cada película y creamos elementos HTML
+        movies.forEach(movie => {
+            const movieItem = document.createElement("li");
+            movieItem.classList.add("item");
+            // Traemos la imagen sino
+            if (movie.backdrop_path) {
+                movieItem.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`;
+            } else {
+                movieItem.style.backgroundImage = `url('img/bg-banner.jpg')`;
+            }
+            // Crea un span para el título de la película
+            const title = document.createElement("span");
+            title.classList.add("title");
+            title.textContent = movie.original_title;
+
+            // Añade el título al elemento de lista
+            movieItem.appendChild(title);
+
+            // Añade el elemento de lista a la lista destacada
+            featuredContainer.appendChild(movieItem);
+        });
+
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+fetchMovies(currentPage)
+
+// Función para cargar la siguiente página
+function nextPage() {
+    currentPage++;
+    fetchMovies(currentPage);
+}
+
+// Función para cargar la página anterior 
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        fetchMovies(currentPage);
+    }
+}
+
+
+//Traemos las peliculas mas aclamadas
+async function fetchTopRated() {
+    try {
+        const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+        const data = await response.json();
+
+        // Aquí extraemos los resultados
+        const TopMovies = data.results;
+        const bestContainer = document.querySelector(".best-container");
+
+        TopMovies.forEach(movie => {
+            const movieItem = document.createElement("li");
+            movieItem.classList.add("item");
+
+            if (movie.poster_path) {
+                movieItem.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`;
+            } else {
+                movieItem.style.backgroundImage = `url('path/to/default/image.jpg')`; // Imagen por defecto si no hay fondo
+            }
+            // Crea un span para el título de la película
+            const title = document.createElement("span");
+            title.classList.add("title");
+            title.textContent = movie.original_title;
+
+            // Añade el título al elemento de lista
+            movieItem.appendChild(title);
+
+            // Añade el elemento de lista a la lista destacada
+            bestContainer.appendChild(movieItem);
+        });
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+fetchTopRated();
+
+/*for (let i = 0; i <19; i++) {
+    //const titles = ["The Beekeeper", "Badland Hunters", "The Marvels", "Wonka", "Aquaman and the Lost Kingdom", "Migration", "Sixty Minutes", "Wish", "The Masked Saint", "Due Justice", "Orion and the Dark", "Genghis Khan", "Lift", "Attack", "Mutant Ghost Wargirl", "Poor Things", "The 5", "Truck: Locked in", "Anyone but you"]
+    const titles=i;
     const movie = document.createElement("li")
     const title = document.createElement("span")
     movie.classList.add("item")
@@ -25,7 +129,7 @@ for (let i = 0; i < 12; i++) {
     title.innerHTML = titles[i]
     movie.appendChild(title)
     best.appendChild(movie)
-}
+}*/
 
 dropdown.addEventListener("click", (e) => {
     const menu = e.target.parentNode.querySelector(".right")
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,26 +1,6 @@
-const dropdown = document.querySelector("header nav .dropdown")
-
-dropdown.addEventListener("click", (e) => {
-    const menu = e.target.parentNode.querySelector(".right")
-    if (menu.style.opacity == "1") {
-        menu.classList.remove("open")
-        menu.classList.add("close")
-        setTimeout(() => {
-            if (menu.classList.contains("close")) {
-                menu.classList.remove("close")
-                menu.style.opacity = "0"
-            }
-        }, 400)
-    } else {
-        menu.classList.remove("close")
-        menu.classList.add("open")
-        menu.style.opacity = "1"
-    }
-})
-
 document.querySelector(".login").addEventListener("submit", (e) => {
     e.preventDefault()
-    const [ email, password ] = Array.from(e.target.querySelectorAll("input")).map((input) => input);
+    const [email, password] = Array.from(e.target.querySelectorAll("input")).map((input) => input);
 
     if (email.value == "" || email.value == undefined || email.value == null || email.value == " " || !/([a-zA-Z0-9]+[@][a-zA-Z]+)[.][a-zA-Z0-9]+/.test(email.value)) {
         email.parentNode.querySelector(".error").classList.remove("hidden")
@@ -29,6 +9,12 @@ document.querySelector(".login").addEventListener("submit", (e) => {
     if (password.value == "" || password.value == undefined || password.value == null || password.value == " " || !/[a-z0-9]{4,}/i.test(password.value)) {
         password.parentNode.querySelector(".error").classList.remove("hidden")
     }
-})
 
-document.querySelectorAll("input").forEach((input) => input.addEventListener("input", (e) => e.target.parentNode.querySelector(".error").classList.add("hidden")))
+    if (email.value.trim() !== "" && password.value.trim() !== "") {
+        Swal.fire({
+            icon: 'success',
+            title: 'Bienvenido',
+            confirmButtonText: 'Ok'
+        });
+    }
+})
